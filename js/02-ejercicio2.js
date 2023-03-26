fba8a4d40f32b2c0b1ee0cc776e8329f61363707
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2023-03-26 09:03:54 -0300

    completo ejercicio 2

--- js/02-ejercicio2.js
+++ js/02-ejercicio2.js
@@ -15,4 +15,32 @@ Un método informar() que retorne la información del estado de la cuenta.
 Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
 
 */
+let cuenta = {
+  titular: "Alex",
+  saldo: 0,
 
+  ingresar: function (cantidad) {
+    cuenta.saldo += cantidad;
+    document.write(`<p>Se ha ingresado ${cantidad} a la cuenta bancaria.</p>`);
+  },
+
+  extraer: function (cantidad) {
+    if (cantidad > cuenta.saldo) {
+      document.write(
+        "No se puede realizar la extraccion por no tener saldo suficuente"
+      );
+    } else {
+      cuenta.saldo -= cantidad;
+      document.write(`<p>Se ha extraido ${cantidad} a la cuenta bancaria.</p>`);
+    }
+  },
+
+  informar: function(){
+    document.write(`<p>El titular ${cuenta.titular} de la cuenta es </p>`)
+    document.write(`<p>El saldo de la cuenta es ${cuenta.saldo} </p>`)
+  }
+};
+
+cuenta.ingresar(100)
+cuenta.extraer(50)
+cuenta.informar()
