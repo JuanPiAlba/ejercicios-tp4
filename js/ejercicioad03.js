94d82075e917f69cd85756a0ad142d62228762be
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2023-03-30 21:51:34 -0300

    agrego ejercicioadicional3

--- js/ejercicioad03.js
+++ js/ejercicioad03.js
@@ -69,3 +69,77 @@ let listaProductos = [
   },
   { nombreProducto: "Agua micellar", precio: 2890, categoria: "Limpieza" },
 ];
+
+function creartabla() {
+  // Crear el elemento de tabla
+  var table = document.createElement("table");
+
+  // Crear la fila de encabezado
+  var headerRow = document.createElement("tr");
+  var header1 = document.createElement("th");
+  var header1Text = document.createTextNode("NombreProducto");
+  header1.appendChild(header1Text);
+  headerRow.appendChild(header1);
+  var header2 = document.createElement("th");
+  var header2Text = document.createTextNode("Precio");
+  header2.appendChild(header2Text);
+  headerRow.appendChild(header2);
+  var header3 = document.createElement("th");
+  var header3Text = document.createTextNode("Categoria");
+  header3.appendChild(header3Text);
+  headerRow.appendChild(header3);
+  table.appendChild(headerRow);
+
+  // Iterar sobre los objetos y crear las filas y celdas correspondientes
+
+  for (var i = 0; i < listaProductos.length; i++) {
+    var row = document.createElement("tr");
+    var cell1 = document.createElement("td");
+    var cell1Text = document.createTextNode(listaProductos[i].nombreProducto);
+    cell1.appendChild(cell1Text);
+    row.appendChild(cell1);
+    var cell2 = document.createElement("td");
+    var cell2Text = document.createTextNode(listaProductos[i].precio);
+    cell2.appendChild(cell2Text);
+    row.appendChild(cell2);
+    var cell3 = document.createElement("td");
+    var cell3Text = document.createTextNode(listaProductos[i].categoria);
+    cell3.appendChild(cell3Text);
+    row.appendChild(cell3);
+    table.appendChild(row);
+  }
+
+  // Agregar la tabla al documento
+  document.body.appendChild(table);
+}
+
+document.write(creartabla());
+
+function filtrarProductosPorCategoria(listaProductos, categoria) {
+  return listaProductos.filter(
+    (listaProductos) => listaProductos.categoria === categoria
+  );
+}
+
+let copiaListaProducto = listaProductos
+
+listaProductos = filtrarProductosPorCategoria(
+  listaProductos,
+  "Protector solar"
+);
+
+document.write(creartabla());
+
+listaProductos = copiaListaProducto
+
+listaProductos = filtrarProductosPorCategoria(listaProductos, "Sérum");
+
+document.write(creartabla());
+
+listaProductos=copiaListaProducto
+listaProductos=filtrarProductosPorCategoria(listaProductos,"Bruma");
+if(listaProductos.length>0){
+  document.write(creartabla());
+  }else{
+    document.write('La categoria bruma no existe')
+  }
